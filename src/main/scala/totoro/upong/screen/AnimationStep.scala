ab636b0fd87bdaed9ffe57afd970c05d480cbb87
MoonlightOwl <murky.owl@gmail.com>
2017-09-19 00:13:27 +0200

    add a little bit of animation

---
+++ src/main/scala/totoro/upong/screen/AnimationStep.scala
@@ -0,0 +1,8 @@
+package totoro.upong.screen
+
+object AnimationStep {
+  val In = 0
+  val Steady = 1
+  val Out = 2
+  val Hidden = 3
+}
--- src/main/scala/totoro/upong/screen/MainMenuScreen.scala
+++ src/main/scala/totoro/upong/screen/MainMenuScreen.scala
@@ -1,6 +1,7 @@
 package totoro.upong.screen
 
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.math.Interpolation
 import totoro.upong.ui.Terminal
 import totoro.upong.{Assets, Config, Upong}
 
@@ -12,19 +13,33 @@ import totoro.upong.{Assets, Config, Upong}
 class MainMenuScreen(game: Upong) extends Screen {
   private val terminal: Terminal = new Terminal(19, 14, Assets.Font.Menu)
 
+  private var step: Int = AnimationStep.In
+  private val interpolation: Interpolation = Interpolation.pow4
+  private var elapsed = 0f
+  private val total = 0.5f
+  private var progress = 0f
+
   terminal.println("Ohayou!")
   terminal.println("This is Upong, mul-tiplayer ping-pong game. Welcome and  enjoy!")
   terminal.println()
   terminal.print("> ")
 
   def render(delta: Float): Unit = {
+    if (step == AnimationStep.In && elapsed < total) {
+      elapsed += delta
+      progress = interpolation.apply(elapsed / total)
+    } else {
+      step = AnimationStep.Steady
+      progress = 1
+    }
+
     terminal.update()
 
     game.batch.begin()
     game.batch.draw(Assets.Tex.Background, 0, 0, 0, 0, Config.Width, Config.Height)
-    game.batch.draw(Assets.Tex.Logo, Config.Width / 2 - 160, Config.Height - 160)
-    game.batch.draw(Assets.Tex.Kururi1, 20, 0)
-    terminal.draw(game.batch, Config.Width / 2 - 20, 480)
+    game.batch.draw(Assets.Tex.Logo, Config.Width / 2 - 160, Config.Height - 160 * progress)
+    game.batch.draw(Assets.Tex.Kururi1, 20, (progress - 1) * 100)
+    terminal.draw(game.batch, Config.Width / 2 - 20, 440 + (40 * progress).toInt)
     game.batch.end()
   }
 
